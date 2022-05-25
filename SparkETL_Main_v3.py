2b21fd04c9cb98f6e01fee570603bfe5b6ba928b
Noam Jacobson <47924194+Noam-Jacob@users.noreply.github.com>
2022-05-25 17:09:26 -0400

    Removed spacing

--- SparkETL_Main_v3.py
+++ SparkETL_Main_v3.py
@@ -13,9 +13,9 @@ from pyspark import SparkConf, SparkContext
 from redShiftFunctions import redShiftQuery, redShiftWrite, redShiftRead
 
 # REPLACE ACCESS KEY AND SECRET KEY in config.py file!
-conf = SparkConf()        .set("spark.hadoop.fs.s3a.access.key", config.s3aAccessKey)        .set("spark.hadoop.fs.s3a.secret.key", config.s3aSecretKey)       .set("spark.hadoop.fs.s3a.endpoint", config.s3aEndpoint)
+conf = SparkConf().set("spark.hadoop.fs.s3a.access.key", config.s3aAccessKey).set("spark.hadoop.fs.s3a.secret.key", config.s3aSecretKey).set("spark.hadoop.fs.s3a.endpoint", config.s3aEndpoint)
 
-spark = SparkSession         .builder         .appName("Main ETL for the Capstone Project")        .config(conf=conf)        .getOrCreate()
+spark = SparkSession.builder.appName("Main ETL for the Capstone Project").config(conf=conf).getOrCreate()
 
 #Used to collect all dataframes.
 tableNamesArray = []
@@ -36,7 +36,7 @@ dfPortCodes = dfPortCodes.filter(~dfPortCodes.name.contains("No PORT") & ~dfPort
 
 #Split the name to columns municipality and statecode.
 nameSplit = split(dfPortCodes.name, ',')
-dfPortCodes = dfPortCodes.withColumn('municipality', trim (lower( nameSplit.getItem(0) ) ) )                          .withColumn('statecode', trim (upper( nameSplit.getItem(1) ) ) )
+dfPortCodes = dfPortCodes.withColumn('municipality', trim (lower( nameSplit.getItem(0) ) ) ).withColumn('statecode', trim (upper( nameSplit.getItem(1) ) ) )
 
 #Remove any rows without state code. Drop column name as its been split to two new columns.
 dfPortCodes = dfPortCodes.filter(~dfPortCodes.statecode.isNull()).drop("name")
@@ -69,7 +69,7 @@ dfAirPorts = dfAirPorts.filter( (dfAirPorts.iso_country == 'US') & (dfAirPorts.t
 latLongSplit = split(dfAirPorts.coordinates, ',')
 isoRegSplit = split(dfAirPorts.iso_region, '-')
 
-dfAirPorts = dfAirPorts.withColumn('longitude', latLongSplit.getItem(0))         .withColumn('latitude', latLongSplit.getItem(1))         .withColumn('statecode', upper(isoRegSplit.getItem(1)))         .withColumn('municipality', lower(dfAirPorts.municipality)) 
+dfAirPorts = dfAirPorts.withColumn('longitude', latLongSplit.getItem(0)).withColumn('latitude', latLongSplit.getItem(1)).withColumn('statecode', upper(isoRegSplit.getItem(1))).withColumn('municipality', lower(dfAirPorts.municipality)) 
 
 #Remove columns continent and iso_country since it is US only.
 dfAirPorts = dfAirPorts.drop("iso_region", "continent", "coordinates", "gps_code", "local_code")
@@ -91,7 +91,7 @@ tableNamesArray.append({'AIRPORTS':dfAirPorts})
 
 dfTemps = spark.read.options(header=True, delimiter=',', inferSchema='True').csv("Datasets/Tempature/GlobalLandTemperaturesByState.csv")
 dfTemps = dfTemps.filter( (dfTemps.Country == 'United States') & (dfTemps.AverageTemperature != 'Nan') )
-dfTemps = dfTemps.withColumn('year', year(dfTemps.dt))           .withColumn('month', month(dfTemps.dt))          .withColumn('dayOfweek', dayofweek(dfTemps.dt))          .withColumn('State', lower (dfTemps.State)) 
+dfTemps = dfTemps.withColumn('year', year(dfTemps.dt)).withColumn('month', month(dfTemps.dt)).withColumn('dayOfweek', dayofweek(dfTemps.dt)).withColumn('State', lower (dfTemps.State)) 
           
 dfTemps = dfTemps.withColumn("id", concat(translate(dfTemps["dt"], "-" , ""), dfTemps.State ))
 dfTemps = dfTemps.drop("dt", "Country")
@@ -111,9 +111,9 @@ tableNamesArray.append({'USATEMP':dfTemps})
 #
 
 dfCityDemo = spark.read.options(header=True, delimiter=';').csv("Datasets/US Demographics/us-cities-demographics.csv")
-dfCityDemo = dfCityDemo.withColumn('City', lower(dfCityDemo.City))                         .withColumn('State', lower(dfCityDemo.State))
+dfCityDemo = dfCityDemo.withColumn('City', lower(dfCityDemo.City)).withColumn('State', lower(dfCityDemo.State))
 
-dfCityDemo = dfCityDemo.withColumnRenamed('City', 'city')                        .withColumnRenamed('State', 'state')                        .withColumnRenamed('Median Age', 'median_age')                        .withColumnRenamed('Male Population', 'male_population')                         .withColumnRenamed('Female Population', 'female_population')                         .withColumnRenamed('Total Population', 'total_population')                         .withColumnRenamed('Number of Veterans', 'number_of_veterans')                         .withColumnRenamed('Foreign-born', 'foreign_born')                         .withColumnRenamed('Average Household Size', 'average_household_size')                         .withColumnRenamed('State Code', 'state_code')                        .withColumnRenamed('Race', 'race')                        .withColumnRenamed('Count', 'count') 
+dfCityDemo = dfCityDemo.withColumnRenamed('City', 'city').withColumnRenamed('State', 'state').withColumnRenamed('Median Age', 'median_age').withColumnRenamed('Male Population', 'male_population').withColumnRenamed('Female Population', 'female_population').withColumnRenamed('Total Population', 'total_population').withColumnRenamed('Number of Veterans', 'number_of_veterans').withColumnRenamed('Foreign-born', 'foreign_born').withColumnRenamed('Average Household Size', 'average_household_size').withColumnRenamed('State Code', 'state_code').withColumnRenamed('Race', 'race').withColumnRenamed('Count', 'count') 
 
 dfCityDemo.createOrReplaceTempView("USADEMOGRAPHICS")
 dfCityDemo = spark.sql("SELECT * FROM USADEMOGRAPHICS WHERE (state_code, city) in (select statecode, municipality from PORTCODES)")
@@ -135,7 +135,7 @@ dfImmigration = spark.read.options(inferSchema='True').parquet("Datasets/Immigra
 
 dfImmigration = dfImmigration.filter(dfImmigration["i94mode"] == 1)
 
-dfImmigration = dfImmigration                 .withColumn("arrdate", regexp_replace(dfImmigration["arrdate"], '\..*$', '').cast(IntegerType()) )                 .withColumn("depdate", regexp_replace(dfImmigration["depdate"], '\..*$', '').cast(IntegerType()) )
+dfImmigration = dfImmigration.withColumn("arrdate", regexp_replace(dfImmigration["arrdate"], '\..*$', '').cast(IntegerType()) ).withColumn("depdate", regexp_replace(dfImmigration["depdate"], '\..*$', '').cast(IntegerType()) )
 
 
 dfImmigration.createOrReplaceTempView("dateTable")
@@ -152,34 +152,17 @@ dfImmigration = spark.sql("""
                     FROM dateTable T1 
                     """)
 
-dfImmigration = dfImmigration.drop("depdate","i94mode","count", "admnum",                                     "entdepa","entdepd","entdepu","matflag","insnum")
+dfImmigration = dfImmigration.drop("depdate","i94mode","count", "admnum","entdepa","entdepd","entdepu","matflag","insnum")
 
 
 
 
 dfImmigration.createOrReplaceTempView("dateTable1")
 
-#dfImmigration = spark.sql("""
-#                    SELECT cicid, count(*) FROM dateTable1 Group by cicid Having count(*) >1
-#                """)
-#dfImmigration = spark.sql(""" 
-#                    SELECT * FROM ( SELECT count(*) AS CNT, i94port FROM dateTable1 T1 
-#                    GROUP BY 
- #                   i94port HAVING COUNT (*) > 20000
-#                    ) T1
-#                    LEFT JOIN AIRPORTS_VIEW2 PC
-#                    ON 
-#                    T1.i94port = PC.code
- #                   ORDER BY CNT DESC
- #                   """)
 
-
-#df2.printSchema()
 
 dfImmigration.limit(10).toPandas()
 #df2.count()
-
-#tableNamesArray.append({'IMMIGRATION':dfImmigration})
 
 
 # In[ ]:
